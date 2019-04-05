0c8395fccc144f19c5d44c91b0902a53d1af56cd
Michael Zion <noisleahcim@gmail.com>
2019-04-05 16:58:14 +0300

    create a db connector module

---
+++ db-connector/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='db-connector',
+    packages=['db-connector'],
+    description='A DB connector for Decimex services',
+    version='0.1',
+    url='http://github.com/decimex/common-modules',
+    author='noisleahcim',
+    author_email='noisleahcim@gmail.com',
+    keywords=['pip','db','connector']
+    )
